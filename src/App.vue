e0b4c0ea6d2b464d84cfa7ba327769fa1e67d7aa
Paula-front <TU_CORREO_DE_PAULA_FRONT>
2026-06-25 22:07:39 -0400

    chore: configurar proyecto base del portafolio

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from './components/layout/Navbar.vue'
+import FooterSection from './components/layout/FooterSection.vue'
+import HeroSection from './components/sections/HeroSection.vue'
+import AboutSection from './components/sections/AboutSection.vue'
+import SkillsSection from './components/sections/SkillsSection.vue'
+import ProjectsSection from './components/sections/ProjectsSection.vue'
+import ContactSection from './components/sections/ContactSection.vue'
+</script>
+
+<template>
+  <Navbar />
+
+  <main>
+    <HeroSection />
+    <AboutSection />
+    <SkillsSection />
+    <ProjectsSection />
+    <ContactSection />
+  </main>
+
+  <FooterSection />
+</template>
